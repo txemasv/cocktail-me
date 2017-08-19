755d7e1640ae2933c41149ab1518b63f8709262c
txemasv <txemasv@gmail.com>
2017-08-19 22:08:10 +0200

    Fourth-commit:  - Correction: Call API to get Drink details in FragmentDetails now

--- app/src/main/java/org/app/txema/cocktailme/activity/DetailsActivity.java
+++ app/src/main/java/org/app/txema/cocktailme/activity/DetailsActivity.java
@@ -5,19 +5,11 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
 
 import org.app.txema.cocktailme.R;
 import org.app.txema.cocktailme.activity.fragment.DetailsFragment;
 import org.app.txema.cocktailme.model.Drink;
-import org.app.txema.cocktailme.model.DrinksResponse;
-import org.app.txema.cocktailme.rest.ApiClient;
-import org.app.txema.cocktailme.rest.ApiInterface;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 /**
  * Created by Txema on 18/08/2017.
@@ -52,13 +44,6 @@ public class DetailsActivity extends AppCompatActivity {
         assert actionBar != null;
         actionBar.setDisplayHomeAsUpEnabled(true);
 
-        //Declare ApiInterface Service
-        ApiInterface apiService =
-                ApiClient.getClient().create(ApiInterface.class);
-
-        //Declare Call function
-        Call<DrinksResponse> call = apiService.getDrinkById(drink.getIdDrink());
-
         //Screen orientation (one-pane: portrait, two-pane: landscape)
         setScreenOrientation();
 
@@ -66,48 +51,12 @@ public class DetailsActivity extends AppCompatActivity {
         //progressDialogShow();
         //TODO
 
-        //Call server (onResponse, onFailure)
-        call.enqueue(mCall());
+        // Capture the detailsFragment from the activity layout
+        DetailsFragment detailsFrag = (DetailsFragment)
+                (DetailsActivity.this).getSupportFragmentManager().findFragmentById(R.id.detailsFragment);
+        detailsFrag.updateDetailsView(drink);
     }
 
-    private Callback<DrinksResponse> mCall() {
-        return new Callback<DrinksResponse>() {
-            @Override
-            public void onResponse(Call<DrinksResponse> call, Response<DrinksResponse> response) {
-                //wait for debugger (NOT IN RUN MODE!)
-                android.os.Debug.waitForDebugger();
-                Log.d(TAG, "wait for debugger ");
-
-                //get status_code
-                int statusCode = response.code();
-
-                //verify response http (code)
-                //200(OK), 400(BAD_REQUEST), 404(NOT_FOUND)
-                switch (statusCode) {
-                    case 200:
-                        //1. Get items from response
-                        DrinksResponse drinksResponse = response.body();
-                        //2. Capture detailsFragment from the activity layout
-                        DetailsFragment detailsFrag = (DetailsFragment)
-                                (DetailsActivity.this).getSupportFragmentManager().findFragmentById(R.id.detailsFragment);
-                        //3. Set drink information in detailsFragment
-                        if(!drinksResponse.getDrinks().isEmpty()) {
-                            detailsFrag.updateDetailsView(drinksResponse.getDrinks().get(0));
-                        }
-                        break;
-                    default:
-                        Log.e(TAG, "HTTP CODE " + statusCode);
-                        break;
-                }
-            }
-
-            @Override
-            public void onFailure(Call<DrinksResponse> call, Throwable t) {
-                // Log error here since request failed
-                Log.e(TAG, t.toString());
-            }
-        };
-    }
 
     private void setScreenOrientation() {
         //Screen orientation (one-pane: portrait)
--- app/src/main/java/org/app/txema/cocktailme/activity/fragment/DetailsFragment.java
+++ app/src/main/java/org/app/txema/cocktailme/activity/fragment/DetailsFragment.java
@@ -2,6 +2,7 @@ package org.app.txema.cocktailme.activity.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,13 +13,22 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 
 import org.app.txema.cocktailme.R;
+import org.app.txema.cocktailme.activity.DetailsActivity;
 import org.app.txema.cocktailme.model.Drink;
+import org.app.txema.cocktailme.model.DrinksResponse;
+import org.app.txema.cocktailme.rest.ApiClient;
+import org.app.txema.cocktailme.rest.ApiInterface;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by Txema on 18/08/2017.
  */
 
 public class DetailsFragment extends Fragment {
+    private static final String TAG = DetailsActivity.class.getSimpleName();
 
     private TextView nameView;
     private ImageView thumbnailView;
@@ -47,28 +57,66 @@ public class DetailsFragment extends Fragment {
     }
 
     public void updateDetailsView(Drink drink) {
-        if (drink != null) {
-            //Insert drink data into views
-            nameView.setText(drink.getName());
-            try {
-                Glide.with(this).load(drink.getThumbnail()).into(thumbnailView);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            instructionsView.setText(drink.getInstructions());
+        //Declare ApiInterface Service
+        ApiInterface apiService =
+                ApiClient.getClient().create(ApiInterface.class);
 
-            //Add items programmatically to ingredientsList
-            for (String ingredient : drink.getIngredientMeasureList()) {
-                TextView ingredientTextView = new TextView(getContext());
-                ingredientTextView.setText(ingredient.replace("\n", "").replace("\r", ""));
-                ingredientTextView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-                ingredientsListView.addView(ingredientTextView);
+        //Declare Call function
+        Call<DrinksResponse> call = apiService.getDrinkById(drink.getIdDrink());
+
+        //Call server (onResponse, onFailure)
+        call.enqueue(new Callback<DrinksResponse>() {
+            @Override
+            public void onResponse(Call<DrinksResponse> call, Response<DrinksResponse> response) {
+                //wait for debugger (NOT IN RUN MODE!)
+                android.os.Debug.waitForDebugger();
+                Log.d(TAG, "wait for debugger ");
+
+                //get status_code
+                int statusCode = response.code();
+
+                //verify response http (code)
+                //200(OK), 400(BAD_REQUEST), 404(NOT_FOUND)
+                switch (statusCode) {
+                    case 200:
+                        //1. Get items from response
+                        DrinksResponse drinksResponse = response.body();
+                        //2. Capture detailsFragment from the activity layout
+                        if (!drinksResponse.getDrinks().isEmpty()) {
+                            Drink drink = drinksResponse.getDrinks().get(0);
+                            //Insert drink data into views
+                            nameView.setText(drink.getName());
+                            try {
+                                Glide.with(getContext()).load(drink.getThumbnail()).into(thumbnailView);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                            instructionsView.setText(drink.getInstructions());
+
+                            //Add items programmatically to ingredientsList
+                            for (String ingredient : drink.getIngredientMeasureList()) {
+                                TextView ingredientTextView = new TextView(getContext());
+                                ingredientTextView.setText(ingredient.replace("\n", "").replace("\r", ""));
+                                ingredientTextView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                                ingredientsListView.addView(ingredientTextView);
+                            }
+                            TextView ingredientTextView = new TextView(getContext());
+                            ingredientTextView.setText(getString(R.string.glass_type_title) + " " + drink.getGlass());
+                            ingredientTextView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                            ingredientsListView.addView(ingredientTextView);
+                        }
+                        break;
+                    default:
+                        Log.e(TAG, "HTTP CODE " + statusCode);
+                        break;
+                }
             }
-            TextView ingredientTextView = new TextView(getContext());
-            ingredientTextView.setText(getString(R.string.glass_type_title)+ " " + drink.getGlass());
-            ingredientTextView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-            ingredientsListView.addView(ingredientTextView);
-        }
+
+            @Override
+            public void onFailure(Call<DrinksResponse> call, Throwable t) {
+                // Log error here since request failed
+                Log.e(TAG, t.toString());
+            }
+        });
     }
-
 }
